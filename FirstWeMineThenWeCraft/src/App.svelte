d6ec252058677f4bd82babc764e611a570950856
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-03 22:41:03 +0300

    feat(layout): Add Dividers instead of Borders. fix(topbar): Make Topbar opaque.

--- FirstWeMineThenWeCraft/src/App.svelte
+++ FirstWeMineThenWeCraft/src/App.svelte
@@ -5,18 +5,21 @@
 
 <main class="h-screen md:overflow-hidden">
   <div class="grid grid-rows-[auto_1fr_auto]">
-    <header class="sticky top-0 z-10 p-4 shadow-md">
+    <header class="sticky top-0 z-10 p-4 shadow-md backdrop-blur-sm ">
       <div class="flex justify-between items-center">
         <h1 class="h1 text-lg md:text-2xl font-bold">First We Mine, Then We Craft</h1>
         <ThemeSwitch />
       </div>
     </header>
+    <hr class="hr border-t-2" />
     <div class="flex flex-col md:flex-row min-h-screen px-4 pt-4 pb-4 md:pt-0 md:pb-0">
-      <div class="flex flex-col items-center w-full md:w-1/3 p-4 border-b md:border-b-0 md:border-r flex-grow">
+      <div class="flex flex-col items-center w-full md:w-1/3 p-4 flex-grow">
         <!-- TODO: Implement LocalStorage for Blocks Counter -->
         <h1 class="h1 text-lg font-bold mb-4">Blocks Mined: NA</h1>
         <Counter />
       </div>
+      <span class="vr hidden md:block border-l-2" ></span>
+      <hr class="hr block: md:hidden border-t-2" />
       <!-- TODO: Add Upgrades alongside with Filters and Favorites/Bought -->
       <div class="flex flex-col items-center w-full md:w-2/3 p-4 flex-grow">
         <h1 class="h1 text-lg font-bold mb-4">Upgrades</h1>
