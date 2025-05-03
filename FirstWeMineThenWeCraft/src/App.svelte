7fc618d721f9e636b4234129c0a6cf5cb4b61539
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-03 19:13:48 +0300

    feat: Add Basic Layout for the App.
    Add Basic Layout for both Small Screen and Large Screen Devices.

--- FirstWeMineThenWeCraft/src/App.svelte
+++ FirstWeMineThenWeCraft/src/App.svelte
@@ -1,47 +1,21 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/logo.svg'
-  import Counter from './lib/Counter.svelte'
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="flex flex-col md:flex-row min-h-screen px-4 pt-4 pb-4 md:pt-0 md:pb-0">
+    <div class="flex flex-col items-center w-full md:w-1/3 p-4 border-b md:border-b-0 md:border-r flex-grow">
+      <!-- TODO: Implement LocalStorage for Blocks Counter -->
+      <h1 class="text-lg font-bold mb-4">Blocks Mined: NA</h1>
+      <Counter />
+    </div>
+
+    <!-- TODO: Add Upgrades alongside with Filters and Favorites/Bought -->
+    <div class="flex flex-col items-center w-full md:w-2/3 p-4 flex-grow">
+      <h1 class="text-lg font-bold mb-4">Upgrades</h1>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
