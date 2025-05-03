4604e472af864806dac2a636ebc1588abd4d65a3
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-04 01:25:39 +0300

    refactor: Delete dist folder

--- FirstWeMineThenWeCraft/vite.config.js
+++ FirstWeMineThenWeCraft/vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Clicker_Game/',
   plugins: [
     tailwindcss(),
     svelte()
   ],
-  base: '/Clicker_Game/FirstWeMineThenWeCraft/',
 })
