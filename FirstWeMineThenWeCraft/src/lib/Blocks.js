e08ac583b1172ff3a1af01ade28cf367ff3a3ff3
Ghenntoggy1 <roman.gusev@isa.utm.md>
2025-05-04 03:21:24 +0300

    feat(counter): Add Image as Button. Add Various Blocks as Images. Implement LocalStorage to store current state of game.

---
+++ FirstWeMineThenWeCraft/src/lib/Blocks.js
@@ -0,0 +1,22 @@
+export const blocks = [
+    {
+      name: 'cobblestone_block',
+      durability: 3,
+      type: 'stone'
+    },
+    {
+      name: 'debris_block',
+      durability: 5,
+      type: 'stone'
+    },
+    {
+      name: 'grass_block',
+      durability: 1,
+      type: 'dirt'
+    },
+    {
+      name: 'planks_block',
+      durability: 2,
+      type: 'wood'
+    }
+  ];
